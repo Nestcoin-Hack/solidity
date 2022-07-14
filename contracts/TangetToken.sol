fcc70573b4be072a3a286889bc3d505edb0980d1
Abdulhameed123 <yusufhameed125@gmail.com>
2022-07-14 23:24:13 +0100

    first commit

---
+++ contracts/TangetToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+// Learn more about the ERC20 implementation 
+// on OpenZeppelin docs: https://docs.openzeppelin.com/contracts/4.x/erc20
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TangetToken is ERC20 {
+    constructor() ERC20("Tanget ETH Token", "TAN") {
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+}
